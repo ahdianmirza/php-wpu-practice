38dfbc0029cc9563a849351af4c189810c81330a
ahdianmirza <ahdianmirza@gmail.com>
2023-02-17 16:14:47 +0700

    menambahkan file latihan php pengkondisian

--- pertemuan3/latihan1.php
+++ pertemuan3/latihan1.php
@@ -30,6 +30,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Latihan 1</title>
+    <style>
+        .warna-baris {
+            background-color: silver;
+        }
+    </style>
 </head>
 
 <body>
@@ -44,13 +49,17 @@
                 }
                 ?> -->
 
-        <?php for ($i = 1; $i <= 3; $i++) : ?>
-            <tr>
+        <?php for ($i = 1; $i <= 5; $i++) : ?>
+            <?php if ($i % 2 == 0) : ?>
+                <tr class="warna-baris">
+                <?php else : ?>
+                <tr>
+                <?php endif; ?>
                 <?php for ($j = 1; $j <= 5; $j++) : ?>
                     <td><?= "$i, $j"; ?></td>
                 <?php endfor ?>
-            </tr>
-        <?php endfor ?>
+                </tr>
+            <?php endfor ?>
     </table>
 </body>
 
