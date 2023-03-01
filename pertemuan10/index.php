bc6b3612c9c3202d144fb19e208b88ce5322731e
ahdianmirza <ahdianmirza@gmail.com>
2023-03-01 15:03:16 +0700

    menambahkan file latihan insert & delete

--- pertemuan10/index.php
+++ pertemuan10/index.php
@@ -1,6 +1,7 @@
 <?php
 require 'functions.php';
 $mahasiswa = query("SELECT * FROM mahasiswa");
+
 ?>
 
 <!DOCTYPE html>
@@ -15,6 +16,10 @@ $mahasiswa = query("SELECT * FROM mahasiswa");
 
 <body>
     <h1>Daftar Mahasiswa</h1>
+
+    <!-- Menambah data -->
+    <a href="tambah.php">Tambah data mahasiswa</a>
+    <br><br>
 
     <table border="1" cellpadding="10" cellspacing="0">
         <tr>
@@ -33,7 +38,7 @@ $mahasiswa = query("SELECT * FROM mahasiswa");
                 <td><?= $i; ?></td>
                 <td>
                     <a href="">Ubah</a> |
-                    <a href="">Hapus</a>
+                    <a href="hapus.php?id=<?= $row["id"]; ?>" onclick="return confirm('yakin ?')">Hapus</a>
                 </td>
                 <td><img src="img/<?= $row["gambar"]; ?>" width="50"></td>
                 <td><?= $row["nim"] ?></td>
